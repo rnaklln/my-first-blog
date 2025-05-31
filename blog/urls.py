121a7f00053cbfb132b644b588e1519637a87477
ranira <ranira.klln@icloud.com>
2025-05-31 11:24:36 +0300

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post'),
-    #path('', views.home, name='home'),
+    path('', views.post_list, name='post'),
 ]
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'',include('blog.urls')),
+    path(r'',include('blog.urls')),
 ]
