72b4e46f85700b231530110c74a3d4994e6cbf8d
ranira <ranira.klln@icloud.com>
2025-06-14 12:05:41 +0300

    Update

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
+
 
 
 def post_list(request):
