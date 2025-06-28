15060cb5729d78a3d8fd6d1a63261bd965315120
ranira <ranira.klln@icloud.com>
2025-06-28 10:56:36 +0300

    Update

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Post
+from ckeditor.widgets import CKEditorWidget
 
 class PostForm(forms.ModelForm):
+    text = forms.CharField(widget=CKEditorWidget())
+
     class Meta:
         model = Post
-        fields = ['title', 'text',]
+        fields = ['title', 'text']
--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from ckeditor.fields import RichTextField
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    header_image = models.ImageField(blank=True, null=True, upload_to='images/')
     text = RichTextField(blank = True, null = True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
