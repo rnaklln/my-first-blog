1935b7719a3431de638ece8c14cbaa0e34ddbe87
ranira <ranira.klln@icloud.com>
2025-06-28 11:56:10 +0300

    Update

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'django_ckeditor_5',
+    'ckeditor',
+    'ckeditor_uploader'
 ]
 
 MIDDLEWARE = [
