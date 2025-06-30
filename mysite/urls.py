f24fcda719675f309b2c0ccb2e7d76c5fba5db3e
ranira <ranira.klln@icloud.com>
2025-06-30 20:40:51 +0300

    Update

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.flatpages import views as flatpages_views
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,6 +25,8 @@ urlpatterns = [
     path('',include('blog.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('about/', flatpages_views.flatpage, {'url': '/about/'}, name='about'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
