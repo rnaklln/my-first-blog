b4530fa976220071c5089cf4a773064fc624eb63
ranira <ranira.klln@icloud.com>
2025-07-04 01:27:54 +0300

    Update

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from django.db import models
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
+from ckeditor.widgets import CKEditorWidget
 
+from .models import Post
 admin.site.register(Post)
+
+class FlatPageAdmin(FlatPageAdmin):
+    fromfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()  # or your RichTextField
+    text = RichTextUploadingField(blank=True, null=True)
     header_image = models.ImageField(upload_to='images/', blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
